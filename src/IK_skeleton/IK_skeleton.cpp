a90ca6fd642562a001a4086e1bcc603d18c99e17
Damien DIDIER <damien.didier@epita.fr>
2023-10-08 17:41:53 +0200

    Code cleanup and fix edge case when every joints and the target were aligned

--- src/IK_skeleton/IK_skeleton.cpp
+++ src/IK_skeleton/IK_skeleton.cpp
@@ -18,6 +18,8 @@ namespace cgp
         {
             join_positions[i] = rest_pose_global[i].translation;
         }
+        assert_cgp(join_positions.size() >= 2, "IK skeleton need at least 2 joints and " + str(join_positions.size()) + "were given")
+
     }
     
     bool IK_skeleton::is_reachable(vec3 target)
@@ -52,6 +54,27 @@ namespace cgp
         {
             // TODO: need to check if all joints and the target are aligned: maybe not able to converge if that is the
             //  case
+
+            bool all_aligned = true;
+            for (int iter = 2; iter < join_positions.size() && all_aligned; iter++)
+            {
+                vec3 ab = join_positions[iter] - join_positions[0];
+                vec3 ac = join_positions[1] - join_positions[0];
+                all_aligned = (is_equal(norm(cross(ab, ac)), 0));
+            }
+            if (all_aligned)
+            {
+                vec3 ab = target_position - join_positions[0];
+                vec3 ac = join_positions[1] - join_positions[0];
+                all_aligned = (is_equal(norm(cross(ab, ac)), 0));
+                if (all_aligned && join_positions.size() > 2)
+                {
+                    vec3 offset = orthogonal_vector(join_positions[1] - join_positions[0]);
+                    offset = 0.01f * offset;
+                    join_positions[join_positions.size() - 2] += offset;
+                }
+            }
+
             size_t nb_iter = 0;
             vec3 b = join_positions[0];
             float dif = norm(target_position - join_positions[join_positions.size() - 1]);
@@ -82,22 +105,11 @@ namespace cgp
     void IK_skeleton::update_skeleton(float animation_time, skeleton_animation_structure &skeleton)
     {
         int nb_joints = join_positions.size();
-        numarray<numarray<size_t>> children_index(nb_joints);
-        for (int k = 0; k < nb_joints; k++)
-        {
-            for (int iter = 0; iter < nb_joints; iter++)
-            {
-                if (join_parent[iter] == k)
-                    children_index[k].push_back(iter);
-            }
-        }
 
-        numarray<affine_rt> rest_pose_global = skeleton.rest_pose_global();
         // update local geometry
-        numarray<affine_rt> local_geometry(nb_joints);
+        numarray<affine_rt> local_geometry(skeleton.rest_pose_local);
         numarray<affine_rt> global_geometry(nb_joints);
-        local_geometry[0] = rest_pose_global[0];
-        global_geometry[0] = rest_pose_global[0];
+        global_geometry[0] = skeleton.rest_pose_local[0];
 
         for (int k = 1; k < nb_joints; k++)
         {
@@ -107,10 +119,7 @@ namespace cgp
             vec3 rest_local_position = normalize(skeleton.rest_pose_local[k].translation);
             rotation_transform rotation =
                     rotation_transform::from_vector_transform(rest_local_position, ik_local_position);
-            local_geometry[k - 1].rotation = rotation;
-            local_geometry[k].translation = skeleton.rest_pose_local[k].translation;
-            if (k == 1)
-                global_geometry[0] = local_geometry[0];
+            local_geometry[k - 1].rotation = local_geometry[k - 1].rotation * rotation;
             global_geometry = skeleton_local_to_global(local_geometry, join_parent);
         }
         numarray<affine_rt> first_frame = skeleton.animation_geometry_local[skeleton.animation_geometry_local.size() - 1];
